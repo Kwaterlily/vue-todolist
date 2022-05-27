d94df10cc074047c877af1d616c11f819c7c9bc9
liviaAugusta <liviamattoni@gmail.com>
2022-05-27 10:55:14 +0200

    todolist

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
-const elenco = new Vue({
+const NewelencoApp = new Vue({
     el : "#container",
     data : {
+        newElement:  "",
         elenco: [
              { text: "Dare da mangiare al gatto", done: true}, 
-             { text: "Rispondere al presidende", done: false}, 
+             { text: "Rispondere al presidente", done: false}, 
              { text: "Ritirare premio nobel", done: false}, 
              { text: "Comprare croccantini per il gatto", done: true}, 
              { text: "Ringraziare Brad Pitt per i fiori", done: true}, 
              { text: "Fare la spesa", done: false}, 
-             { text: "Declinare offertea della N.a.s.a.", done: false}, 
+             { text: "Declinare offerta della N.a.s.a.", done: false}, 
              { text: "Comprare bottiglia di vino per la cena con Roger Waters e Anna", done: true},        
         ],
     }, 
+    
     methods: {
         aggiungi () {
-            const newTasck = { text: "", done: false }; 
-            this.elenco.push ("")
+            if (this.newElement !== "")  {
+                
+                const newTasck = { text: this.newElement, done: false };
+                this.elenco.push(newTasck);
+                this.newElement = "";
+            }
         },
+
+        togliElemento (index) {
+            this.elenco.splice(index,1); 
+        },
+
+        invertDone (index) {
+            if (this.elenco[index].done === true) {
+                this.elenco[index].done = false
+            }   else {
+                this.elenco[index].done = true
+            }
+        }
+
     }
-  
+});  
